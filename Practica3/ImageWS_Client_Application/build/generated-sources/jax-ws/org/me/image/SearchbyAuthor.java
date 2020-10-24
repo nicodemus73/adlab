d14d135c023c200aa0604c05555d546bec3cc78d
Samuel Roca <samuel.samuel.roca3@gmail.com>
2020-10-24 16:02:08 +0200

    ...

---
+++ Practica3/ImageWS_Client_Application/build/generated-sources/jax-ws/org/me/image/SearchbyAuthor.java
@@ -0,0 +1,60 @@
+
+package org.me.image;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para SearchbyAuthor complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="SearchbyAuthor">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="author" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "SearchbyAuthor", propOrder = {
+    "author"
+})
+public class SearchbyAuthor {
+
+    protected String author;
+
+    /**
+     * Obtiene el valor de la propiedad author.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getAuthor() {
+        return author;
+    }
+
+    /**
+     * Define el valor de la propiedad author.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setAuthor(String value) {
+        this.author = value;
+    }
+
+}
---
+++ Practica3/ImageWS_Client_Application/build/generated/jax-wsCache/ImageWS/org/me/image/SearchbyCreaDate.java
@@ -0,0 +1,60 @@
+
+package org.me.image;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para SearchbyCreaDate complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType name="SearchbyCreaDate">
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="creaDate" type="{http://www.w3.org/2001/XMLSchema}string" minOccurs="0"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "SearchbyCreaDate", propOrder = {
+    "creaDate"
+})
+public class SearchbyCreaDate {
+
+    protected String creaDate;
+
+    /**
+     * Obtiene el valor de la propiedad creaDate.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getCreaDate() {
+        return creaDate;
+    }
+
+    /**
+     * Define el valor de la propiedad creaDate.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setCreaDate(String value) {
+        this.creaDate = value;
+    }
+
+}
